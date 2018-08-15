a8efde2a1564b09a21fdad0ff050ff73be952f61
马齐 <much19940917@gmail.com>
2018-08-15 09:38:03 +0800

    提交ingnore

--- app/src/main/java/com/xiaomakj/hellogcc/MainActivity.java
+++ app/src/main/java/com/xiaomakj/hellogcc/MainActivity.java
@@ -2,6 +2,7 @@ package com.xiaomakj.hellogcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TestJniMethods methods = new TestJniMethods();
+
         new MuchJNIShow().muchShow(19);
         //android.os.Debug.waitForDebugger();
         methods.test();
-        setTitle(methods.nativeMethod());
+        String title = methods.nativeMethod();
+        setTitle(title);
+//        Log.i("MainActivity's title===",title);
         methods.newJniThreads(5);    //创建5个线程
 //        Object obj = TestJniMethods.allocNativeBuffer(16L); //分配字节缓冲区
 //        methods.freeNativeBuffer(obj);  //释放字节缓冲区
